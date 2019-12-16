9e32b485c428661395240f4e5769459db22140b3
Charlie Saunders <charlieasaunders@gmail.com>
2019-12-15 22:27:03 -0500

    Separate parts 1 and 2 for day 5.

--- src/day5.rs
+++ src/day5.rs
@@ -14,6 +14,17 @@ pub fn part1(initial_program: &[i32]) -> i32 {
     program.copy_from_slice(initial_program);
 
     let mut outputs = Vec::with_capacity(1);
+    run_program(&mut program, vec![1], &mut outputs);
+    outputs[outputs.len() - 1]
+}
+
+#[aoc(day5, part2)]
+pub fn part2(initial_program: &[i32]) -> i32 {
+    let mut program = Vec::new();
+    program.resize(initial_program.len(), 0);
+    program.copy_from_slice(initial_program);
+
+    let mut outputs = Vec::with_capacity(1);
     run_program(&mut program, vec![5], &mut outputs);
     outputs[0]
 }
