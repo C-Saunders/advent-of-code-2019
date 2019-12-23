057caa2e522a77289a5928ada268b68ff70c80fb
Charlie Saunders <charlieasaunders@gmail.com>
2019-12-23 11:48:55 -0800

    Complete day 8 part 2.

--- src/day8.rs
+++ src/day8.rs
@@ -19,8 +19,12 @@ impl OccurrenceCounts {
 }
 
 #[aoc_generator(day8)]
-pub fn get_input_rows(input: &str) -> Vec<Vec<u32>> {
-    let chunks = input.chars().chunks(25 * 6);
+pub fn generator(input: &str) -> Vec<Vec<u32>> {
+    get_image_as_layers(input, 25, 6)
+}
+
+fn get_image_as_layers(input: &str, width: usize, height: usize) -> Vec<Vec<u32>> {
+    let chunks = input.chars().chunks(width * height);
     chunks
         .into_iter()
         .map(|chunk| chunk.map(|i| i.to_digit(10).unwrap()).collect())
@@ -44,8 +48,6 @@ pub fn part1(image: &Vec<Vec<u32>>) -> u32 {
         }
     }
 
-    dbg!(&counts_with_fewest_zeros);
-
     counts_with_fewest_zeros.ones * counts_with_fewest_zeros.twos
 }
 
@@ -63,6 +65,43 @@ fn get_counts_for_layer(layer: &Vec<u32>) -> OccurrenceCounts {
     result
 }
 
+#[aoc(day8, part2)]
+pub fn part2(image: &Vec<Vec<u32>>) -> String {
+    make_image(image, 25, 6)
+}
+
+fn make_image(image_data: &Vec<Vec<u32>>, width: usize, height: usize) -> String {
+    let mut image_as_line: Vec<Option<u32>> = vec![None; width * height];
+
+    for index in 0..image_data[0].len() {
+        for layer in image_data {
+            if image_as_line[index].is_some() {
+                continue;
+            }
+            if layer[index] != 2 {
+                image_as_line[index] = Some(layer[index]);
+            }
+        }
+    }
+
+    let image_as_string: String = image_as_line
+        .iter()
+        .map(|item| match item {
+            Some(0) => ' ',
+            Some(1) => '#',
+            _ => panic!("what?"),
+        })
+        .collect();
+
+    let mut output = "\n".to_string();
+    for chunk in &image_as_string.chars().chunks(width) {
+        output.push_str(&chunk.collect::<String>());
+        output.push_str("\n");
+    }
+
+    output
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -76,6 +115,14 @@ mod tests {
                 ones: 5,
                 twos: 3
             }
+        );
+    }
+
+    #[test]
+    fn image_test() {
+        assert_eq!(
+            make_image(&get_image_as_layers(&"0222112222120000", 2, 2), 2, 2),
+            "\n #\n# \n"
         );
     }
 }
