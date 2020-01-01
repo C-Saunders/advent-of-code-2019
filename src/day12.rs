31448b33acfe9f3da1201a2a84e9ba4644a549fd
Charlie Saunders <charlieasaunders@gmail.com>
2020-01-01 14:01:07 -0500

    Implement a brute force solution to day 12.

--- src/day12.rs
+++ src/day12.rs
@@ -2,8 +2,11 @@ use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
 use regex::Regex;
 use std::cell::RefCell;
+use std::collections::hash_map::DefaultHasher;
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct ThreeDimensionalValue {
     x: i32,
     y: i32,
@@ -16,7 +19,7 @@ impl ThreeDimensionalValue {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub struct Moon {
     position: ThreeDimensionalValue,
     velocity: ThreeDimensionalValue,
@@ -38,8 +41,45 @@ impl Moon {
     }
 }
 
+#[derive(PartialEq, Eq)]
+pub struct MoonSystem {
+    io: RefCell<Moon>,
+    europa: RefCell<Moon>,
+    ganymede: RefCell<Moon>,
+    callisto: RefCell<Moon>,
+}
+
+impl MoonSystem {
+    fn len(&self) -> usize {
+        4
+    }
+
+    fn get_by_index(&self, index: usize) -> &RefCell<Moon> {
+        match index {
+            0 => &self.io,
+            1 => &self.europa,
+            2 => &self.ganymede,
+            3 => &self.callisto,
+            _ => panic!("No moon at index"),
+        }
+    }
+
+    fn as_vec(&self) -> Vec<&RefCell<Moon>> {
+        vec![&self.io, &self.europa, &self.ganymede, &self.callisto]
+    }
+}
+
+impl Hash for MoonSystem {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.io.borrow().hash(state);
+        self.europa.borrow().hash(state);
+        self.ganymede.borrow().hash(state);
+        self.callisto.borrow().hash(state);
+    }
+}
+
 #[aoc_generator(day12)]
-pub fn get_starting_positions(input: &str) -> Vec<RefCell<Moon>> {
+pub fn get_starting_positions(input: &str) -> MoonSystem {
     lazy_static! {
         static ref PARSING_EXPR: Regex =
             Regex::new(r"^<x=(?P<x>-?\d+), y=(?P<y>-?\d+), z=(?P<z>-?\d+)>$").unwrap();
@@ -56,47 +96,49 @@ pub fn get_starting_positions(input: &str) -> Vec<RefCell<Moon>> {
         })
         .collect();
 
-    return vec![
-        RefCell::new(Moon {
+    return MoonSystem {
+        io: RefCell::new(Moon {
             position: positions[0],
             velocity: ThreeDimensionalValue::zeros(),
         }),
-        RefCell::new(Moon {
+        europa: RefCell::new(Moon {
             position: positions[1],
             velocity: ThreeDimensionalValue::zeros(),
         }),
-        RefCell::new(Moon {
+        ganymede: RefCell::new(Moon {
             position: positions[2],
             velocity: ThreeDimensionalValue::zeros(),
         }),
-        RefCell::new(Moon {
+        callisto: RefCell::new(Moon {
             position: positions[3],
             velocity: ThreeDimensionalValue::zeros(),
         }),
-    ];
+    };
 }
 
 #[aoc(day12, part1)]
-pub fn part1(moons: &[RefCell<Moon>]) -> i32 {
+pub fn part1(moons: &MoonSystem) -> i32 {
     let ticks = 1000;
 
     for _ in 0..ticks {
         update_velocities(&moons);
-        for moon in moons {
+        for moon in moons.as_vec().iter() {
             moon.borrow_mut().apply_velocity();
         }
     }
 
-    moons.iter().fold(0, |acc, curr| {
+    moons.as_vec().iter().fold(0, |acc, curr| {
         let moon = curr.borrow();
         acc + moon.potential_energy() * moon.kinetic_energy()
     })
 }
 
-fn update_velocities(moons: &[RefCell<Moon>]) {
+fn update_velocities(moons: &MoonSystem) {
     for moon_pair_indexes in (0..(moons.len())).combinations(2) {
-        let mut a = moons[moon_pair_indexes[0]].borrow_mut();
-        let mut b = moons[moon_pair_indexes[1]].borrow_mut();
+        let a_ref = moons.get_by_index(moon_pair_indexes[0]);
+        let b_ref = moons.get_by_index(moon_pair_indexes[1]);
+        let mut a = a_ref.borrow_mut();
+        let mut b = b_ref.borrow_mut();
         if a.position.x < b.position.x {
             a.velocity.x += 1;
             b.velocity.x -= 1;
@@ -123,7 +165,28 @@ fn update_velocities(moons: &[RefCell<Moon>]) {
     }
 }
 
-// #[aoc(day12, part2)]
-// pub fn part2(program_input: &[ThreeDimensionalPoint]) -> usize {
-//     0
-// }
+#[aoc(day12, part2)]
+pub fn part2(moons: &MoonSystem) -> usize {
+    let mut seen = HashSet::<u64>::new();
+    seen.insert(calculate_hash(&moons));
+
+    loop {
+        update_velocities(&moons);
+        for moon in moons.as_vec().iter() {
+            moon.borrow_mut().apply_velocity();
+        }
+        let curr_hash = calculate_hash(&moons);
+        if seen.contains(&curr_hash) {
+            break;
+        }
+        seen.insert(curr_hash);
+    }
+
+    seen.len()
+}
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
